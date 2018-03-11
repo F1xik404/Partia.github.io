2ba352bbf3c26aa0179b7aa63a476eeefe619c4f
Partia <illianovosad28@gmail.com>
2018-03-11 19:04:48 +0200

    Eror

--- dani.js
+++ dani.js
@@ -17,28 +17,28 @@ if (user == 148804){
 if (user == 148805){
     document.write("Hello Kira Yednak");
     break;
-    if (user == 148806){
+if (user == 148806){
     document.write("Hello Artem Galashevskiy");
     break;
-    if (user == 148807){
+if (user == 148807){
     document.write("Hello Dercash Yaroslav");
     break;
-    if (user == 148808){
+if (user == 148808){
     document.write("Hello Dima Tkachuk");
     break;
-    if (user == 148809){
+if (user == 148809){
     document.write("Hello Dovgai Roma");
     break;
     }
-     if (user == 148810){
+if (user == 148810){
     document.write("Hello Kramar Solomia");
     break;
     }
-     if (user == 148811){
+if (user == 148811){
     document.write("Hello Shcarovecka Liza");
     break;
     }
-     if (user == 148812){
+if (user == 148812){
     document.write("Hello Antoshchuk Misha");
     break;
     }
